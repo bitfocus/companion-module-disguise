ed6735333cfb3f71f51a70d784dd39ae57945fa1
William Viker <william@trippelm.no>
2019-04-28 17:29:33 +0200

    Implemented more OSC commands for disguise

--- disguise.js
+++ disguise.js
@@ -71,22 +71,75 @@ instance.prototype.actions = function(system) {
 		'play_to_end': { label: 'Play to end of section' },
 		'loop_section': { label: 'Loop section' },
 		'stop': { label: 'Stop' },
-		'previous_section': { label: 'Previous Section' },
-		'next_section': { label: 'Next Section' },
-		'previous_track': { label: 'Previous track' },
-		'next_track': { label: 'Next track' },
-		'fade_up': { label: 'Master brightness - Fade up' },
-		'fade_down': { label: 'Master brightness - Fade down' },
-		'cue': {
-			label: 'Trigger cue',
+		'return_to_start': {
+			label: 'Return to start'
+		},
+
+		'previous_section': { label: 'Section - Previous' },
+		'next_section': { label: 'Section - Next' },
+
+		'previous_track': { label: 'Track - Previous' },
+		'next_track': { label: 'Track - Next' },
+
+		'track_id': {
+			label: 'Goto - Track ID',
 			options: [{
 				type: 'textinput',
-				label: 'Cue number',
+				label: 'Track ID',
+				id: 'track_id',
+				default: '0',
+				regex: self.REGEX_NUMBER
+			}]
+		},
+
+		'track_name': {
+			label: 'Goto - Track Name',
+			options: [{
+				type: 'textinput',
+				label: 'Track Name',
+				id: 'track_name',
+				default: 'track',
+				regex: self.REGEX_SOMETHING
+			}]
+		},
+
+		'cue': {
+			label: 'Goto - CUE',
+			options: [{
+				type: 'textinput',
+				label: 'CUE number',
 				id: 'cue',
 				default: '0',
 				regex: self.REGEX_NUMBER
 			}]
-		}
+		},
+
+		'brightness': {
+			label: 'Master Brightness - Set',
+			options: [{
+				type: 'textinput',
+				label: 'Brightness (0-100)',
+				id: 'brightness',
+				default: '100',
+				regex: self.REGEX_PERCENT
+			}]
+		},
+
+		'fade_up': { label: 'Master Brightness - Fade up' },
+		'fade_down': { label: 'Master Brightness - Fade down' },
+
+		'volume': {
+			label: 'Master Volume - Set',
+			options: [{
+				type: 'textinput',
+				label: 'Volume (0-100)',
+				id: 'volume',
+				default: '100',
+				regex: self.REGEX_PERCENT
+			}]
+		},
+
+
 	});
 
 }
@@ -100,24 +153,53 @@ instance.prototype.action = function(action) {
 	debug('run action:', id);
 
 	var osc = {
+
 		'play': '/d3/showcontrol/play',
 		'play_to_end': '/d3/showcontrol/playsection',
 		'loop_section': '/d3/showcontrol/loop',
 		'stop': '/d3/showcontrol/stop',
+
 		'previous_section': '/d3/showcontrol/previoussection',
 		'next_section': '/d3/showcontrol/nextsection',
+
+		'track_id': '/d3/showcontrol/trackid',
+		'track_name': '/d3/showcontrol/trackname',
+
 		'previous_track': '/d3/showcontrol/previoustrack',
 		'next_track': '/d3/showcontrol/nexttrack',
+
+		'brightness': '/d3/showcontrol/brightness',
 		'fade_up': '/d3/showcontrol/fadeup',
 		'fade_down': '/d3/showcontrol/fadedown',
-		'cue': '/d3/showcontrol/cue'
+
+		'cue': '/d3/showcontrol/cue',
+		'return_to_start': '/d3/showcontrol/returntostart',
+		'volume': '/d3/showcontrol/volume'
+
 	};
 
 	if (id == 'cue') {
 		args.push({ type: 'f', value: action.options.cue });
 	}
 
+	else if (id == 'track_id') {
+		args.push({ type: 'i', value: action.options.track_id });
+	}
+
+	else if (id == 'track_name') {
+		args.push({ type: 's', value: action.options.track_name });
+	}
+
+	else if (id == 'volume') {
+		args.push({ type: 'f', value: (parseInt(action.options.volume) / 100) });
+	}
+
+	else if (id == 'brightness') {
+		args.push({ type: 'f', value: (parseInt(action.options.brightness) / 100) });
+	}
+
 	if (osc[id] !== undefined) {
+		console.log("send osc", osc[id],args)
 		self.system.emit('osc_send', self.config.host, port, osc[id], args);
 	}
 
